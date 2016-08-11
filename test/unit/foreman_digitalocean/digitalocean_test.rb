9a5e0bcee43cf478320823ab82cda47c46346f87
Daniel Lobato <elobatocs@gmail.com>
2016-08-11 17:32:56 +0200

    Fixes #16071 - New/edit compute resource doesn't remove API_key
    The 'unset' field depends on core on 'password' in order to not remove
    it and set a placeholder. It could be fixed there to be more flexible
    but since we're using alias_attribute api_key, password this is fine.

--- test/unit/foreman_digitalocean/digitalocean_test.rb
+++ test/unit/foreman_digitalocean/digitalocean_test.rb
@@ -1,16 +1,17 @@
 require 'test_plugin_helper'
 
-class ForemanDigitalocean::DigitaloceanTest < ActiveSupport::TestCase
-  should validate_presence_of(:api_key)
-  should delegate_method(:flavors).to(:client)
-  should have_one(:key_pair)
+module ForemanDigitalocean
+  class DigitaloceanTest < ActiveSupport::TestCase
+    should validate_presence_of(:api_key)
+    should have_one(:key_pair)
 
-  setup { Fog.mock! }
-  teardown { Fog.unmock! }
+    setup { Fog.mock! }
+    teardown { Fog.unmock! }
 
-  test 'ssh key pair gets created after its saved' do
-    digitalocean = FactoryGirl.build(:digitalocean_cr)
-    digitalocean.expects(:setup_key_pair)
-    digitalocean.save
+    test 'ssh key pair gets created after its saved' do
+      digitalocean = FactoryGirl.build(:digitalocean_cr)
+      digitalocean.expects(:setup_key_pair)
+      digitalocean.save
+    end
   end
 end
